2337027e093cf090320c845930833a00fc231c5e
CoderX <devop0b111@gmail.com>
2025-10-15 11:52:37 +0530

    add some code

--- git.sh
+++ git.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 echo "What you want to do !"
+options= "
+         git add 
+         git commit 
+
+ if you want to add a file just give this option with file name 
+   
+     ad file.txt 
+
+
+then for commit enter the commit message only 
+
+     add code in file 
+
+"
+
+
 echo "-h  or --help"
 help=" {
          git add . ( if add all files )
